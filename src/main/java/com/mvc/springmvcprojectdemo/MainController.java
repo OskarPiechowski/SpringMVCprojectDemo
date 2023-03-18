6340e22c47bdda1c66ce8c1d9e0c5ade65962c82
Oskar <o.piechowski@gmail.com>
2023-03-18 12:13:52 +0100

    css and js made.

--- src/main/java/com/mvc/springmvcprojectdemo/MainController.java
+++ src/main/java/com/mvc/springmvcprojectdemo/MainController.java
@@ -3,12 +3,14 @@ package com.mvc.springmvcprojectdemo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
 
     @RequestMapping(path = "/imperator",method = RequestMethod.GET)
-    public void getImperatorPage(){
-        System.out.println("Odpalamy stronkę z zarządzanymi flotami imperium");
+    public ModelAndView getImperatorPage(){
+        ModelAndView modelAndView = new ModelAndView("imperator-page.html");
+        return modelAndView;
     }
 }
