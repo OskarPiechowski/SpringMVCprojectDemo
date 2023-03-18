f5cc323e28e2e05f5f81a365817793cad2b080aa
Oskar <o.piechowski@gmail.com>
2023-03-18 11:32:00 +0100

    git commit

---
+++ src/main/java/com/mvc/springmvcprojectdemo/springMvcProjectDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mvc.springmvcprojectdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class springMvcProjectDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(springMvcProjectDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mvc/springmvcprojectdemo/SpringMvCprojectDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvc.springmvcprojectdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvCprojectDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
